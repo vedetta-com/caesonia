143b4926d077f8b0a6bbe804d90ee4144872df60
Horia Racoviceanu <horia@racoviceanu.com>
2018-04-14 16:15:33 -0400

    Update learn_all_spam.sh

--- src/usr/local/bin/learn_all_spam.sh
+++ src/usr/local/bin/learn_all_spam.sh
@@ -8,13 +8,13 @@ set -o nounset
 for USER in $(doveadm user '*'); do
   echo "Loading $USER"
 
-  doveadm search -u $USER mailbox Spam ALL |
+  doveadm search -u "$USER" mailbox Spam ALL |
 
   while read guid uid; do
-    doveadm -f pager fetch -u $USER text mailbox-guid $guid uid $uid \
-      | sed '1d;$d' \
-      | rspamc -h /var/run/rspamd/rspamd.sock -d $USER learn_spam
-    done
+    doveadm -f pager fetch -u "$USER" text mailbox-guid "$guid" uid "$uid" \
+    | sed '1d;$d' \
+    | rspamc -h /var/run/rspamd/rspamd.sock -d "$USER" learn_spam
+  done
 
 done
 
