ad42658c64dad9f4c80b30e0e540886b33538741
Horia Racoviceanu <horia@racoviceanu.com>
2018-05-04 14:17:36 -0400

    Fix typo

--- src/usr/local/bin/wks-server.sh
+++ src/usr/local/bin/wks-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 exec /usr/local/bin/gpg-wks-server -v --receive \
-        --header X-WKS-Loop=wks.caesonia.com \
+        --header X-WKS-Loop=wks.example.com \
         --from "${1}" \
         --send
 # we don't care about the exit status in this case
