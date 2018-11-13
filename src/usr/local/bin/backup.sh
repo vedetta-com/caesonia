0675868aaf5c1e1f2e171af2fa4a3277adc13ae9
Horia Racoviceanu <horia@racoviceanu.com>
2018-11-13 14:57:36 -0500

    [Fix #140] Remove relic backup.sh

--- src/usr/local/bin/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-DIR="/home/puffy"
-ARC="mercury.tar.gz"
-
-tar -czv -f "${DIR}"/backup/"${ARC}" -I "${DIR}"/backup/include
-
-#tar -C "${DIR}"/backup/extract -xzpf "${DIR}"/backup/"${ARC}"
